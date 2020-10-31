b155d38f8fac1a1871048c775b1243e7a3907c18
lordnordical <stengleinderian2@gmail.com>
2020-10-31 00:24:20 -0400

    Added new function to change the nickname of the victim on command.

--- libbot.py
+++ libbot.py
@@ -3,9 +3,11 @@ import os
 import random
 
 from discord.embeds import Embed
+from discord.member import Member
+from discord.guild import Guild
 
 # Constant for the Discord UID
-UID = 335130747612561418
+UID = 0
 
 class MyClient(discord.Client):
     enable = False
@@ -21,15 +23,27 @@ class MyClient(discord.Client):
         Returns a message easy
         """
         # only sends a message if the command user isnt the bot itself
+        channel = message.channel
         if message.author == self.user:
             return
         elif message.author.id == UID:
             tokens = message.content.split(' ')
             if message.content.startswith("$"):
+                # temporary override when nobody is around to guinea pig for me and i have to test myself
+                if message.content == "$override":
+                    await message.channel.send("Overriden. Torture mode enabled.")
+                    self.enable = True
+                elif message.content == "$doverride":
+                    await message.channel.send("Overriden. Torture mode disabled.")
+                    self.enable = False
                 await message.channel.send("I don't listen to you lol")
             else:
                 if self.enable == True:
-                    await message.channel.send("K.")
+                    num = random.randint(1, 2)
+                    if num == 1:
+                        await message.channel.send("K.")
+                    else:
+                        await message.channel.send(":neutral_face:")
             if random.randint(1, 15) == 15 and self.enable == True:
                 await message.channel.send("Deleting message loser")
                 await message.delete()
@@ -38,24 +52,28 @@ class MyClient(discord.Client):
             tokens: List[str] = message.content[1:].split(' ')
             if self.enable is False:
                 if tokens[0] == "enable":
-                    await message.channel.send("Torture mode enabled. Get ready Miles.")
+                    await message.channel.send("Torture Mode enabled. Get ready Miles.")
                     self.enable = True
                 elif tokens[0] == 'help':
                     await option(message.channel)
                 elif tokens[0] == 'status':
-                    await message.channel.send("I'm up and running, and ready to torture!")
+                    await message.channel.send("I'm up and running, Torture Mode disabled.")
                 else:
-                    await message.channel.send("Torture mode is disabled. Type $enable to turn it on, or type $help.")
+                    await message.channel.send("Torture Mode is disabled. Type $enable to turn it on, or type $help.")
             else:
                 if tokens[0] == 'help':
                         await option(message.channel)
                 elif tokens[0] == 'hello':
                     await message.channel.send("Hello!")
                 elif tokens[0] == "disable":
-                    await message.channel.send("Disabling torture mode.")
+                    await message.channel.send("Disabling Torture Mode.")
                     self.enable = False
                 elif tokens[0] == 'status':
                     await message.channel.send("I'm up and running, and ready to torture!")
+                elif tokens[0] == 'nickname':
+                    user = await channel.guild.fetch_member(UID)
+                    await nick_change(user, channel.guild)
+                    await message.channel.send("Nickname changed successfully!")
                 else:
                     await message.channel.send("Invalid command. Check your spelling or type $help for a list of options")
 
@@ -68,25 +86,39 @@ async def option(channel) -> None:
         color=0xf76902
     )
     embed_var.add_field(
-        name="$hello",
-        value="Echos hello!",
+        name="$status",
+        value="Confirms that I'm up and running, and displays the status of Torture Mode.",
         inline=False
     )
     embed_var.add_field(
         name="$enable",
-        value="Enables torture mode.",
+        value="Enables Torture Mode.",
         inline=False
     )
     embed_var.add_field(
         name="$disable",
-        value="Disables torture mode.",
+        value="Disables Torture Mode.",
         inline=False
     )
     embed_var.add_field(
-        name="$status",
-        value="Confirms that I'm up and running.",
+        name="$hello",
+        value="Echos hello! (Torture Mode required)",
+        inline=False
+    )
+    embed_var.add_field(
+        name="$nickname",
+        value="Changes the nickname of the victim wink wink. (Torture Mode required)",
         inline=False
     )
     await channel.send(embed=embed_var)
+
+async def nick_change(user, guild):
+    """
+    Randomly changes the nickname of the victim when called.
+    """
+    nick_list = []
+    num = random.randint(0, len(nick_list) - 1)
+    return await user.edit(nick=nick_list[num])
+    
 
 client.run(os.getenv('LIBBOT_API_KEY', ''))
